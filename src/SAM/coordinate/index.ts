37884ef3ade50963eb2182ec846dcb9627b1a2f1
Seongho <bluetosh7@gmail.com>
2024-05-16 15:25:46 +0900

    fix: name; inclination -> polar

--- src/SAM/coordinate/index.ts
+++ src/SAM/coordinate/index.ts
@@ -7,12 +7,12 @@ interface SphericalCoordinateOptions {
 
 /*
  * Spherical coordinate system.
- * Start destination is (-1, 0, 0); inclination is 0; azimuth is 0.
+ * Start destination is (-1, 0, 0); polar is 0; azimuth is 0.
  */
 export class SphericalCoordinate {
   origin: SAM.Vector3;
   radius: number;
-  inclination: number;
+  polar: number;
   azimuth: number;
   maxInclination: number;
   minInclination: number;
@@ -20,22 +20,22 @@ export class SphericalCoordinate {
   constructor(options?: SphericalCoordinateOptions) {
     this.origin = new SAM.Vector3([0, 0, 0]);
     this.radius = 1;
-    this.inclination = 0;
+    this.polar = 0;
     this.azimuth = 0;
     this.minInclination = options?.minInclination;
     this.maxInclination = options?.maxInclination;
   }
 
-  addInclination(inclination: number): void {
-    this.inclination = SAM.normalizeAngle(this.inclination + inclination);
+  addInclination(polar: number): void {
+    this.polar = SAM.normalizeAngle(this.polar + polar);
 
-    if (this.maxInclination != null && this.inclination > this.maxInclination) {
-      this.inclination = this.maxInclination;
+    if (this.maxInclination != null && this.polar > this.maxInclination) {
+      this.polar = this.maxInclination;
     } else if (
       this.minInclination != null &&
-      this.inclination < this.minInclination
+      this.polar < this.minInclination
     ) {
-      this.inclination = this.minInclination;
+      this.polar = this.minInclination;
     }
   }
 
@@ -48,28 +48,28 @@ export class SphericalCoordinate {
 
     this.origin = origin;
     this.radius = destFromOrigin.getLength();
-    this.inclination = Math.asin(destFromOrigin.getY() / this.radius);
+    this.polar = Math.asin(destFromOrigin.getY() / this.radius);
     this.azimuth = Math.atan2(destFromOrigin.getZ(), destFromOrigin.getX());
   }
 
   getDestination(): SAM.Vector3 {
-    const x = this.radius * Math.cos(this.inclination) * Math.cos(this.azimuth);
-    const y = this.radius * Math.sin(this.inclination);
-    const z = this.radius * Math.cos(this.inclination) * Math.sin(this.azimuth);
+    const x = this.radius * Math.cos(this.polar) * Math.cos(this.azimuth);
+    const y = this.radius * Math.sin(this.polar);
+    const z = this.radius * Math.cos(this.polar) * Math.sin(this.azimuth);
 
     const vectorInSphere = new SAM.Vector3([x, y, z]);
     return this.origin.add(vectorInSphere);
   }
 
   getUp(): SAM.Vector3 {
-    const x = -Math.sin(this.inclination) * Math.cos(this.azimuth);
-    const y = Math.cos(this.inclination);
-    const z = -Math.sin(this.inclination) * Math.sin(this.azimuth);
+    const x = -Math.sin(this.polar) * Math.cos(this.azimuth);
+    const y = Math.cos(this.polar);
+    const z = -Math.sin(this.polar) * Math.sin(this.azimuth);
 
     return new SAM.Vector3([x, y, z]);
   }
 
   toString(): string {
-    return `origin: ${this.origin.toString()}, radius: ${this.radius}, inclination: ${this.inclination}, azimuth: ${this.azimuth}`;
+    return `origin: ${this.origin.toString()}, radius: ${this.radius}, polar: ${this.polar}, azimuth: ${this.azimuth}`;
   }
 }
