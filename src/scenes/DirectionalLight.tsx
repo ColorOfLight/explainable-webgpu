0eb95f2563fd96337e5c535c8b5f683388b885b5
Seongho <bluetosh7@gmail.com>
2024-05-21 12:34:00 +0900

    feat: add scene for directional light

---
+++ src/scenes/DirectionalLight.tsx
@@ -0,0 +1,62 @@
+import { useRef, useEffect } from "react";
+import * as SAM from "@site/src/SAM";
+
+const drawCanvas = async (canvas: HTMLCanvasElement) => {
+  const renderer = new SAM.WebGPURenderer(canvas);
+  await renderer.init();
+
+  const scene = new SAM.Scene();
+
+  const material = new SAM.PhongMaterial({
+    color: new SAM.Color([1, 1, 0]),
+  });
+
+  const cubeGeometry = new SAM.SphereGeometry(0.5);
+
+  const cube = new SAM.Mesh(cubeGeometry, material);
+
+  scene.add(cube);
+
+  const light1 = new SAM.AmbientLight(new SAM.Color([1, 1, 1]), 0.4);
+  scene.add(light1);
+
+  const light2 = new SAM.DirectionalLight(
+    new SAM.Color([0, 1, 1]),
+    0.2,
+    new SAM.Vector3([0, 0, -1])
+  );
+  scene.add(light2);
+
+  const camera = new SAM.PerspectiveCamera(
+    Math.PI / 2,
+    canvas.clientWidth / canvas.clientHeight,
+    0.1,
+    100
+  );
+
+  camera.eye = new SAM.Vector3([0, 0, 1]);
+
+  const orbitalControl = new SAM.OrbitalControl(canvas);
+  orbitalControl.attachTo(camera);
+
+  const resizeObserver = renderer.generateResizeObserver();
+  resizeObserver.observe(canvas);
+
+  SAM.runTick(() => {
+    cube.setRotateY(0.01);
+
+    renderer.render(scene, camera);
+  });
+};
+
+const DirectionalLightScene = () => {
+  const canvasRef = useRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    drawCanvas(canvasRef.current);
+  }, []);
+
+  return <canvas width="500" height="500" ref={canvasRef} />;
+};
+
+export default DirectionalLightScene;
