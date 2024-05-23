0bf11fbfe67b878b4af5dbdc15e3c56de8f4972b
Seongho <bluetosh7@gmail.com>
2024-05-23 15:30:38 +0900

    feat: add TextureScene

---
+++ src/scenes/Texture.tsx
@@ -0,0 +1,70 @@
+import { useRef, useEffect } from "react";
+import * as SAM from "@site/src/SAM";
+
+import Brick12Png from "/textures/brick_12/brick_12-128x128.png";
+
+const drawCanvas = async (canvas: HTMLCanvasElement) => {
+  const renderer = new SAM.WebGPURenderer(canvas);
+  await renderer.init();
+
+  const texture = new SAM.ImageTexture(Brick12Png);
+  await texture.load();
+
+  const scene = new SAM.Scene();
+
+  const material = new SAM.ImageTextureMaterial(texture);
+
+  const cubeGeometry = new SAM.CubeGeometry(0.5, 0.5, 0.5);
+  const sphereGeometry = new SAM.SphereGeometry(0.3);
+  const planeGeometry = new SAM.PlaneGeometry(1, 1);
+
+  const cube = new SAM.Mesh(cubeGeometry, material);
+  const sphere = new SAM.Mesh(sphereGeometry, material);
+  const plane = new SAM.Mesh(planeGeometry, material);
+
+  cube.setTranslate(new SAM.Vector3([-0.5, -0.5, 0]));
+  sphere.setTranslate(new SAM.Vector3([0, 0.5, 0]));
+  plane.setTranslate(new SAM.Vector3([0.5, -0.5, 0]));
+
+  scene.add(cube);
+  scene.add(sphere);
+  scene.add(plane);
+
+  const light1 = new SAM.AmbientLight(new SAM.Color([1, 1, 1]), 1);
+  scene.add(light1);
+
+  const camera = new SAM.PerspectiveCamera(
+    Math.PI / 2,
+    canvas.clientWidth / canvas.clientHeight,
+    0.1,
+    100
+  );
+
+  camera.eye = new SAM.Vector3([0, 0, 1.5]);
+
+  const orbitalControl = new SAM.OrbitalControl(canvas);
+  orbitalControl.attachTo(camera);
+
+  const resizeObserver = renderer.generateResizeObserver();
+  resizeObserver.observe(canvas);
+
+  SAM.runTick(() => {
+    cube.setRotateY(0.01);
+    sphere.setRotateY(0.01);
+    plane.setRotateY(0.01);
+
+    renderer.render(scene, camera);
+  });
+};
+
+const TextureScene = () => {
+  const canvasRef = useRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    drawCanvas(canvasRef.current);
+  }, []);
+
+  return <canvas width="500" height="500" ref={canvasRef} />;
+};
+
+export default TextureScene;
