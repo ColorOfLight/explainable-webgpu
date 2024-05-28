79607d9ede810746674def9ec131a57ad709500f
Seongho <bluetosh7@gmail.com>
2024-05-28 16:54:36 +0900

    feat: apply watchKeys for the other camera bindDatas

--- src/SAM_NEW/SceneElement/CameraElement.ts
+++ src/SAM_NEW/SceneElement/CameraElement.ts
@@ -2,12 +2,14 @@ import * as SAM from "@site/src/SAM_NEW";
 
 import { NodeElement } from "./_base";
 
-export class CameraElement extends NodeElement<SAM.Camera> {
+export class CameraElement<
+  C extends SAM.Camera = SAM.Camera,
+> extends NodeElement<C> {
   buffers: GPUBuffer[];
   bindGroup: GPUBindGroup;
   bindGroupLayout: GPUBindGroupLayout;
 
-  constructor(device: GPUDevice, camera: SAM.Camera) {
+  constructor(device: GPUDevice, camera: C) {
     super(device, camera);
 
     const bindDataList = this.getBindDataList(camera);
@@ -22,36 +24,73 @@ export class CameraElement extends NodeElement<SAM.Camera> {
     this.bindGroupLayout = bindGroupLayout;
   }
 
-  getBindDataList(camera: SAM.Camera): SAM.BindData<SAM.Camera>[] {
-    return [
-      {
-        label: "viewTransform",
-        visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
-        data: {
-          type: "float32Array",
-          getValue: () =>
-            new Float32Array(camera.getViewTransformMatrix().toRenderingData()),
-        },
-        watchKeys: ["eye", "target", "up"],
+  getBindDataList(camera: C): SAM.BindData<C>[] {
+    const viewTransformBindData = {
+      label: "viewTransform",
+      visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+      data: {
+        type: "float32Array" as const,
+        getValue: () =>
+          new Float32Array(camera.getViewTransformMatrix().toRenderingData()),
       },
-      {
+      watchKeys: ["eye", "target", "up"] as (keyof C)[],
+    };
+
+    const eyeVectorBindData = {
+      label: "eyeVector",
+      visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+      data: {
+        type: "float32Array" as const,
+        getValue: () => new Float32Array(camera.getEyeVector().toNumberArray()),
+      },
+      watchKeys: ["eye"] as (keyof C)[],
+    };
+
+    if (camera instanceof SAM.PerspectiveCamera) {
+      const projectionTransformBindData = {
         label: "projTransform",
         visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
         data: {
-          type: "float32Array",
+          type: "float32Array" as const,
           getValue: () =>
             new Float32Array(camera.getProjTransformMatrix().toRenderingData()),
         },
-      },
-      {
-        label: "eyeVector",
+        watchKeys: ["fov", "aspect", "near", "far"] as (keyof C)[],
+      };
+
+      return [
+        viewTransformBindData,
+        projectionTransformBindData,
+        eyeVectorBindData,
+      ];
+    }
+
+    if (camera instanceof SAM.OrthographicCamera) {
+      const projectionTransformBindData = {
+        label: "projTransform",
         visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
         data: {
-          type: "float32Array",
+          type: "float32Array" as const,
           getValue: () =>
-            new Float32Array(camera.getEyeVector().toNumberArray()),
+            new Float32Array(camera.getProjTransformMatrix().toRenderingData()),
         },
-      },
-    ];
+        watchKeys: [
+          "left",
+          "right",
+          "top",
+          "bottom",
+          "near",
+          "far",
+        ] as (keyof C)[],
+      };
+
+      return [
+        viewTransformBindData,
+        projectionTransformBindData,
+        eyeVectorBindData,
+      ];
+    }
+
+    throw new Error("Unsupported camera type");
   }
 }
