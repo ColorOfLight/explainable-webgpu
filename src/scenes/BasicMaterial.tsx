ae96294b0d259714728895be3fe0c4b2957c4071
Seongho <bluetosh7@gmail.com>
2024-05-29 17:57:21 +0900

    feat: use SAM_NEW for BasicMaterialScene

--- src/scenes/BasicMaterial.tsx
+++ src/scenes/BasicMaterial.tsx
@@ -1,35 +1,31 @@
 import { useRef, useEffect } from "react";
-import * as SAM from "@site/src/SAM";
+import * as SAM from "@site/src/SAM_NEW";
 
 const drawCanvas = async (canvas: HTMLCanvasElement) => {
   const renderer = new SAM.WebGPURenderer(canvas);
   await renderer.init();
 
-  const scene = new SAM.Scene();
+  const sceneManager = renderer.createSceneManager();
 
   const geometry = new SAM.CubeGeometry(0.5, 0.5, 0.5, {
     colors: {
-      front: new SAM.Color([0.5, 0.5, 0]),
-      back: new SAM.Color([0.5, 0, 0.5]),
-      top: new SAM.Color([0, 0.5, 0.5]),
-      bottom: new SAM.Color([0.5, 0, 0]),
-      left: new SAM.Color([0, 0.5, 0]),
-      right: new SAM.Color([0, 0, 0.5]),
+      front: new SAM.Color(0.5, 0.5, 0),
+      back: new SAM.Color(0.5, 0, 0.5),
+      top: new SAM.Color(0, 0.5, 0.5),
+      bottom: new SAM.Color(0.5, 0, 0),
+      left: new SAM.Color(0, 0.5, 0),
+      right: new SAM.Color(0, 0, 0.5),
     },
   });
 
   const mesh1 = new SAM.Mesh(
     geometry,
-    new SAM.BasicMaterial({ color: new SAM.Color([1, 0, 0]) })
+    new SAM.BasicMaterial(undefined, { color: new SAM.Color(1, 0, 0) })
   );
-  mesh1.setTranslate(new SAM.Vector3([-0.5, 0, 0]));
-
-  scene.add(mesh1);
+  mesh1.setTranslate(new SAM.Vector3(-0.5, 0, 0));
 
   const mesh2 = new SAM.Mesh(geometry, new SAM.BasicMaterial());
-  mesh2.setTranslate(new SAM.Vector3([0.5, 0, 0]));
-
-  scene.add(mesh2);
+  mesh2.setTranslate(new SAM.Vector3(0.5, 0, 0));
 
   const camera = new SAM.PerspectiveCamera(
     Math.PI / 2,
@@ -38,7 +34,11 @@ const drawCanvas = async (canvas: HTMLCanvasElement) => {
     100
   );
 
-  camera.eye = new SAM.Vector3([0, 0, 1]);
+  camera.eye = new SAM.Vector3(0, 0, 1);
+
+  sceneManager.add(mesh1);
+  sceneManager.add(mesh2);
+  sceneManager.add(camera);
 
   const orbitalControl = new SAM.OrbitalControl(canvas);
   orbitalControl.attachTo(camera);
@@ -50,7 +50,7 @@ const drawCanvas = async (canvas: HTMLCanvasElement) => {
     mesh1.setRotateX(0.01);
     mesh2.setRotateY(0.01);
 
-    renderer.render(scene, camera);
+    renderer.render(sceneManager, camera);
   });
 };
 
