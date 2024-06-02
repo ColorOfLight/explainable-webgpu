36db6b2b9bcf5af3eaae760dd9e0efa219650bc8
Seongho <bluetosh7@gmail.com>
2024-06-02 18:55:16 +0900

    feat: update AmbientLightScene using SAM_NEW

--- src/scenes/AmbientLight.tsx
+++ src/scenes/AmbientLight.tsx
@@ -1,27 +1,27 @@
 import { useRef, useEffect } from "react";
-import * as SAM from "@site/src/SAM";
+import * as SAM from "@site/src/SAM_NEW";
 
 const drawCanvas = async (canvas: HTMLCanvasElement) => {
   const renderer = new SAM.WebGPURenderer(canvas);
   await renderer.init();
 
-  const scene = new SAM.Scene();
+  const sceneManager = renderer.createSceneManager();
 
   const material = new SAM.SimpleStandardMaterial({
-    color: new SAM.Color([1, 1, 1]),
+    color: new SAM.Color(1, 1, 1),
   });
 
   const cubeGeometry = new SAM.CubeGeometry(0.5, 0.5, 0.5);
 
   const cube = new SAM.Mesh(cubeGeometry, material);
 
-  scene.add(cube);
+  sceneManager.add(cube);
 
-  const light1 = new SAM.AmbientLight(new SAM.Color([1, 1, 0]), 0.5);
-  scene.add(light1);
+  const light1 = new SAM.AmbientLight(new SAM.Color(1, 1, 0), 0.5);
+  sceneManager.add(light1);
 
-  const light2 = new SAM.AmbientLight(new SAM.Color([1, 0, 1]), 0.2);
-  scene.add(light2);
+  const light2 = new SAM.AmbientLight(new SAM.Color(1, 0, 1), 0.2);
+  sceneManager.add(light2);
 
   const camera = new SAM.PerspectiveCamera(
     Math.PI / 2,
@@ -29,8 +29,9 @@ const drawCanvas = async (canvas: HTMLCanvasElement) => {
     0.1,
     100
   );
+  camera.eye = new SAM.Vector3(0, 0, 1);
 
-  camera.eye = new SAM.Vector3([0, 0, 1]);
+  sceneManager.add(camera);
 
   const orbitalControl = new SAM.OrbitalControl(canvas);
   orbitalControl.attachTo(camera);
@@ -41,7 +42,7 @@ const drawCanvas = async (canvas: HTMLCanvasElement) => {
   SAM.runTick(() => {
     cube.setRotateY(0.01);
 
-    renderer.render(scene, camera);
+    renderer.render(sceneManager, camera);
   });
 };
 
